f5b07fcfe164bf1abfd6401248015d3c9bd6f196
JustAgent <ivan.baykov.03@mail.ru>
2022-07-04 10:50:52 +0300

    added dependencies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
-import abi from './Vote.json'
+//import abi from './Vote.json'
+import abi from 'contracts/Vote.json'
 
 export const contractABI = abi.abi;
-
-export const contractAddress = '0xc78D96cA943b0A282E6dfaA5481c06B54e7cCca0' 
+//export const contractAddress = '0xc78D96cA943b0A282E6dfaA5481c06B54e7cCca0' 
+export const contractAddress = abi.networks[5].address 
